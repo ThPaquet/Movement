2122c78a6c427af18468ac64321f22205d00fcfa
ThPaquet <Th_Paquet@hotmail.com>
2022-02-12 13:34:01 -0500

    controlleurs utilisateur et intersection

---
+++ Movement/Movement.API/Controllers/IntersectionController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movement.Services.DepotInterfaces;
+using Movement.Services.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntersectionController : ControllerBase
+    {
+        private readonly IDepotIntersection _depot;
+
+        public IntersectionController(IDepotIntersection p_depot)
+        {
+            this._depot = p_depot;
+        }
+        // GET: api/<IntersectionController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Intersection>> Get()
+        {
+            return Ok(this._depot.GetAll()); 
+        }
+
+        // GET api/<IntersectionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<IntersectionController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Intersection p_intersection)
+        {
+            if (p_intersection == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            this._depot.Post(p_intersection);
+            return Created(nameof(this.Post), p_intersection);
+        }
+
+        // PUT api/<IntersectionController>/5
+        [HttpPut("{p_id}")]
+        public ActionResult Put(int p_id, [FromBody] Intersection p_intersection)
+        {
+            if (p_intersection == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Intersection intersection = this._depot.GetAll()
+                .FirstOrDefault(b => b.Id == p_id);
+
+            if (intersection == null)
+            {
+                return NotFound();
+            }
+
+            intersection.Nom = p_intersection.Nom;
+            intersection.Bornes = p_intersection.Bornes;
+
+            this._depot.Put(intersection);
+            return NoContent();
+        }
+    }
+}
---
+++ Movement/Movement.API/Controllers/UtilisateurController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movement.Services.DepotInterfaces;
+using Movement.Services.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateurController : ControllerBase
+    {
+        private readonly IDepotUtilisateur _depot;
+
+        public UtilisateurController(IDepotUtilisateur p_depot)
+        {
+            this._depot = p_depot;
+        }
+        // GET: api/<UtilisateurController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Utilisateur>> Get()
+        {
+            return Ok(this._depot.GetAll());
+        }
+
+        // GET api/<UtilisateurController>/5
+        [HttpGet("{p_id}")]
+        public ActionResult<Utilisateur> Get(int p_id)
+        {
+            Utilisateur utilisateur = this._depot.GetUtilisateurById(p_id);
+
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(utilisateur);
+        }
+
+        // POST api/<UtilisateurController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Utilisateur p_utilisateur)
+        {
+            if (p_utilisateur == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            this._depot.Post(p_utilisateur);
+
+            return Created(nameof(this.Post), p_utilisateur);
+        }
+
+        // PUT api/<UtilisateurController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int p_id, [FromBody] Utilisateur p_utilisateur)
+        {
+            if (p_utilisateur == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Utilisateur utilisateur = this._depot.GetUtilisateurById(p_id);
+
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            utilisateur.TypeUtilisateur = p_utilisateur.TypeUtilisateur;
+            utilisateur.Cle = p_utilisateur.Cle;
+
+            this._depot.Put(utilisateur);
+            return NoContent();
+        }
+    }
+}
