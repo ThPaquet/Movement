474bfbb3c5e54fe6e9d543801c1e6ad959ba7de7
Sebastien Bedard <2095110@csfoy.ca>
2022-02-12 00:14:46 -0500

    Ajout Movement.Services

---
+++ Movement/Movement.Services/Entities/TraversesPieton.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movement.Data
+{
+    public class TraversesPieton
+    {
+        public int TraversesPietonID { get; set; }
+        public int IntersectionID { get; set; }
+        public int TypeTransportID { get; set; }
+        public DateTime Date { get; set; }
+        public string Direction { get; set; }
+        public int Delais { get; set; }
+
+        public TraversesPieton()
+        {
+
+        }
+    }
+}
+
