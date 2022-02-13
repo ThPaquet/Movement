c1d9181c0e1733ed9cfb6b0449696253639f9ab0
Jonathan Blouin <blouinjonathan07@gmail.com>
2022-02-12 19:37:27 -0500

    api get test

--- Movement/Movement.API/Controllers/ActionFeuController.cs
+++ Movement/Movement.API/Controllers/ActionFeuController.cs
@@ -2,6 +2,7 @@
 using Movement.Services.DepotInterfaces;
 using Movement.Services.Entities;
 using Newtonsoft.Json;
+using Movement_API;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,10 +27,21 @@ namespace Movement.API.Controllers
 
         // GET api/<ActionFeuController>/5
         [HttpGet("{p_requete}")]
-        public string Get([FromBody] Requete p_requete)
+        public ActionResult<float> Get(string p_requete)
         {
-            // mlnet
-            return "value";
+            var sampleData = new MLModel.ModelInput()
+            {
+                TypeUtilisateur = 3,
+                Date = "2021-04-10 07:10:35",
+                LongitudeBorneDepart = 46.846234f,
+                LongitudeBorneArrivee = 46.846272f,
+                LatitudeBorneDepart = -71.37293f,
+                LatitudeBorneArrivee = -71.37293f
+            };
+
+            var result = MLModel.Predict(sampleData);
+
+            return Ok(result.Score);
         }
 
         // POST api/<ActionFeuController>
